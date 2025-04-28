0a95fa5799315c3d7b5e05fbb0c72ffbf0a2595e
Patrick Wilson <patrick@pwry.net>
2025-04-28 13:34:04 -0500

    Use new command to download Scoop

--- download-scoop.ps1
+++ download-scoop.ps1
@@ -1,2 +1,2 @@
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
